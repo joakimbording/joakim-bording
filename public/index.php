9bf9eb34d46f57c965ad5d934faa21de786d12c3
Joakim Bording <joakim@netliferesearch.com>
2017-06-14 17:54:53 +0200

    Added new content for skills, case and contact

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-	$versionSlug = 'v1.1';
+	$versionSlug = 'v1.2';
 	$introText = 'Rådgiver, designer, utvikler og kursholder. Jeg kan hjelpe deg med å nå dine forretningsmål gjennom design. Jeg jobber som frilanser i Oslo og er åpen for oppdrag.';
 ?>
 <!doctype html>
@@ -39,43 +39,100 @@
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQJS8BZ"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
-  <main class="page">
-		<div class="page__visual">
-			<picture class="visual">
-				<source media="(min-width: 720px)" srcset="/images/portrait_joakim_bording.jpg 1x">
-				<img src="/images/landscape_joakim_bording.jpg" data-object-fit="cover" alt="Portrett av Joakim Bording" class="visual__image">
-			</picture>
+	<main>
+	  <div class="card">
+			<div class="card__visual">
+				<picture class="visual">
+					<source media="(min-width: 720px)" srcset="/images/portrait_joakim_bording.jpg 1x">
+					<img src="/images/landscape_joakim_bording.jpg" data-object-fit="cover" alt="Portrett av Joakim Bording" class="visual__image">
+				</picture>
+			</div>
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h1>Joakim Bording</h1>
+					<p><?php echo $introText; ?></p>
+					<dl class="info-list">
+						<div class="info-list__item info-list__item--large">
+							<div class="info info--large">
+								<dt class="info__title">E-post: </dt>
+								<dd class="info__value"><a href="mailto:joakim@bording.no" aria-label="E-post: joakim@bording.no">joakim@bording.no</a></dd>
+							</div>
+						</div>
+						<div class="info-list__item">
+							<div class="info">
+								<dt class="info__title">Telefon: </dt>
+								<dd class="info__value"><a href="tel:91814458" aria-label="Telefon: 918 14 458">918 14 458</a></dd>
+							</div>
+						</div>
+						<div class="info-list__item">
+							<div class="info">
+								<dt class="info__title">Twitter: </dt>
+								<dd class="info__value"><a href="https://twitter.com/spispeas" aria-label="Twitter brukernavn: @spispeas">@spispeas</a></dd>
+							</div>
+						</div>
+						<div class="info-list__item">
+							<div class="info">
+								<dt class="info__title">LinkedIn: </dt>
+								<dd class="info__value"><a href="http://no.linkedin.com/pub/joakim-bording/0/932/6a9/no"  aria-label="LinkedIn profil">Profil</a></dd>
+							</div>
+						</div>
+					</dl>
+				</div>
+			</div>
+	  </div>
+		<div class="card card--triple">
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h2>Strategi fra et brukerperspektiv</h2>
+					<p>Jeg har gjort over 150 brukertester på tvers av bransjer og målgrupper. Jeg forstår hvordan du kan nå dine mål på brukernes premisser.</p>
+				</div>
+			</div>
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h2>Design og front-end utvikling</h2>
+					<p>Jeg kan realisere ideer. Design og teknologi er nært knyttet. Jeg har lang erfaring innen webutvikling på et profesjonelt nivå..
+				</div>
+			</div>
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h2>Kurs og foredrag</h2>
+					<p>Jeg elsker å forelese. Jeg har tidligere vært fagleder på deltidsstudier i interaksjonsdesign og pratet på konferanser om universell utforming. </p>
+				</div>
+			</div>
 		</div>
-		<div class="page__content">
-			<h1>Joakim Bording</h1>
-			<p><?php echo $introText; ?></p>
-			<dl class="info-list">
-				<div class="info-list__item info-list__item--large">
-					<div class="info info--large">
-						<dt class="info__title">E-post: </dt>
-						<dd class="info__value"><a href="mailto:joakim@bording.no" aria-label="E-post: joakim@bording.no">joakim@bording.no</a></dd>
-					</div>
-				</div>
-				<div class="info-list__item">
+		<div class="card">
+			<div class="card__visual">
+				<picture class="visual">
+					<source media="(min-width: 720px)" srcset="/images/obos_merkevare.jpg 1x">
+					<img src="/images/obos_merkevare.jpg" data-object-fit="cover" alt="obos.no screenshet" class="visual__image">
+				</picture>
+			</div>
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h2 class="h1"><small>Case: </small>OBOS Merkevare</h2>
+					<p>Vi moderniserte OBOS konsernet sin visuelle identitet på digitale flater. Identiteten ble utformet og dokumentert i en nettbasert merkevarehåndbok. Designsystemet og front-end koden som jeg utviklet for styleguiden har blitt brukt videre for å redesigne <a href="https://www.obos.no/">obos.no</a>.</p>
+					<p>Jeg hadde ansvar for interaksjonsdesign og front-end utvikling i prosjektet. Jeg hadde den primære kundekontakten med OBOS og fulgte dem opp på implementering av designet.</p>
+					<p><small>Prosjektet ble gjort våren 2017 i <a href="https://www.netliferesearch.com/">Netlife Research</a> sammen med <a href="https://www.netliferesearch.com/eivind-molvaer">Eivind Molvær</a> og <a href="https://www.linkedin.com/in/anette-mork-8b23a81b/">Anette Mork</a>.</small></p>
 					<div class="info">
-						<dt class="info__title">Telefon: </dt>
-						<dd class="info__value"><a href="tel:91814458" aria-label="Telefon: 918 14 458">918 14 458</a></dd>
+						<span class="info__title">Se resultatet:</span>
+						<span class="info__value"><a href="https://merkevare.obos.no/">merkevare.obos.no</a></span>
 					</div>
 				</div>
-				<div class="info-list__item">
-					<div class="info">
-						<dt class="info__title">Twitter: </dt>
-						<dd class="info__value"><a href="https://twitter.com/spispeas" aria-label="Twitter brukernavn: @spispeas">@spispeas</a></dd>
-					</div>
-				</div>
-				<div class="info-list__item">
-					<div class="info">
-						<dt class="info__title">LinkedIn: </dt>
-						<dd class="info__value"><a href="http://no.linkedin.com/pub/joakim-bording/0/932/6a9/no"  aria-label="LinkedIn profil">Profil</a></dd>
-					</div>
-				</div>
-			</dl>
+			</div>
 		</div>
-  </main>
-
+		<div class="card card--flip">
+			<div class="card__visual">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2000.066470442301!2d10.737523616106998!3d59.914444181867374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e7d14f254e9%3A0xae4ee237206e6a58!2sRosenkrantz&#39;+gate+10%2C+0159+Oslo!5e0!3m2!1sno!2sno!4v1497454725900" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+			<div class="card__content">
+				<div class="card__text-frame">
+					<h2 class="h1">La oss ta en kaffe!</h2>
+					<p>Det er mange gode kafeér ikke langt fra kontoret mitt for eksempel?</p>
+					<div class="info">
+						<span class="info__title">Telefon: </dt>
+						<span class="info__value"><a href="tel:91814458" aria-label="Telefon: 918 14 458">918 14 458</a></dd>
+					</div>
+				</div>
+			</div>
+	</main>
 </body>
